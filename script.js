aadbaa0a16b48cad19df60ecfd35a3a915e8a6db
IDZTPHKKKS <shyaamchess@gmail.com>
2023-06-23 22:20:19 +0530

    added files

---
+++ script.js
@@ -0,0 +1,64 @@
+function help()
+            {
+                window.location.assign("https://help.netflix.com/en/");
+            }
+function investor()
+            {
+                window.location.assign("https://ir.netflix.net/ir-overview/profile/default.aspx");
+            }
+function use()
+            {
+                window.location.assign("https://help.netflix.com/legal/termsofuse");
+            }
+function corp()
+            {
+                window.location.assign("https://help.netflix.com/legal/corpinfo");
+            }
+function legal()
+            {
+                window.location.assign("https://help.netflix.com/legal/notices");
+            }
+            function speed()
+            {
+                window.location.assign("https://fast.com/");
+            }
+function cookie()
+            {
+                window.location.assign("https://www.allrecipes.com/gallery/most-popular-types-of-cookies/");
+            }
+            function watch()
+            {
+                window.location.assign("https://devices.netflix.com/en/");
+            }
+function media()
+{
+    window.location.assign("https://media.netflix.com/en/");
+}
+function contact()
+{
+    window.location.assign("https://help.netflix.com/en/contactus");
+}
+function only()
+{
+    window.location.assign("https://www.netflix.com/in/browse/genre/839338");
+}
+function privacy()
+{
+    window.location.assign("https://help.netflix.com/legal/privacy");
+}
+function jobs()
+{
+    window.location.assign("https://jobs.netflix.com/");
+}
+function jobs()
+{
+    window.location.assign("https://jobs.netflix.com/");
+}
+function account()
+{
+    window.location.assign("https://www.netflix.com/in/login?nextpage=https%3A%2F%2Fwww.netflix.com%2Fyouraccount");
+}
+function faq()
+{
+    window.location.assign("https://help.netflix.com/en/node/412");
+}          
